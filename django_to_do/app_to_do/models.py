b558834184f669c60ce9bf881326e0b76efb256a
Radosław Wierzgała <rad.wierzgala@gmail.com>
2022-12-03 22:17:36 +0100

    Tworzenie interfejsu v1

--- django_to_do/app_to_do/models.py
+++ django_to_do/app_to_do/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title + " <object id:" + str(self.id) + ">"
--- django_to_do/app_to_do/views.py
+++ django_to_do/app_to_do/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 def index(request):
-    return render(request, 'index.html')
+    tasks = Task.objects.all().values
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'index.html', context)
